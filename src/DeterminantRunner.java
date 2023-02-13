05eb30d57f7b77d58072fe7803ae1a6ac2b19715
harlanhowe <harlan.howe@kinkaid.org>
2023-02-12 18:27:27 -0600

    Improved documentation on reduceMatrix. Refactored/extracted printMatrix method.

--- src/DeterminantRunner.java
+++ src/DeterminantRunner.java
@@ -16,12 +16,7 @@ public class DeterminantRunner
         int[][] matrix = generateMatrix(N);
 
         System.out.println("Matrix:");
-        for (int[] row: matrix)
-        {
-            for (int x: row)
-                System.out.printf("%4d ",x);
-            System.out.println("");
-        }
+        printMatrix(matrix);
 
         System.out.println("Copy and paste this into Wolfram Alpha:");
         System.out.print("determinant of {");
@@ -45,7 +40,21 @@ public class DeterminantRunner
     }
 
     /**
-     * Builds an n x n 2-d array with random integers in the range [-100,100)
+     * prints out the given matrix in a grid.
+     * @param matrixToPrint
+     */
+    public static void printMatrix(int [][] matrixToPrint)
+    {
+        for (int[] row: matrixToPrint)
+        {
+            for (int x: row)
+                System.out.printf("%4d ",x);
+            System.out.println("");
+        }
+    }
+
+    /**
+     * Builds an n x n 2-d array with random integers in the range (-100,100)
      * @param n size of the array
      * @return a 2-d array of integers
      */
@@ -54,7 +63,7 @@ public class DeterminantRunner
         int[][] result = new int[n][n];
         for (int r=0; r<n; r++)
             for (int c=0; c<n; c++)
-                result[r][c] = (int)(Math.random()*200-100);
+                result[r][c] = (int)(Math.random()*199-99);
         return result;
     }
 
@@ -71,8 +80,27 @@ public class DeterminantRunner
     }
 
     /**
-     * Given an n x n 2-d array of integers, generates a n-1 x n-1 2-d array of integers by omitting the given row and
-     * column
+     * Given an (n x n) 2-d array of integers, generates an (n-1 x n-1) 2-d array of integers by omitting the given row
+     * and column
+     *
+     * For example, if you had a matrix
+     *          1  2  3  4  5
+     *          6  7  8  9  10
+     *    A =   11 12 13 14 15
+     *          16 17 18 19 20
+     *          21 22 23 24 25
+     *
+     * then B = reduceMatrix(A, 0, 0) would give
+     *          7  8  9  10
+     *    B =   12 13 14 15
+     *          17 18 19 20
+     *          22 23 24 25
+     * and C = reduceMatrix(A, 1, 2) would give
+     *         1  2  4  5
+     *     C = 11 12 14 15
+     *         16 17 19 20
+     *         21 22 24 25
+     *
      * @param source the 2-d array of integers
      * @param rowToRemove the index of the row to omit
      * @param colToRemove the index of the column to omit
